367e7d624870d57f96ec820211f002a93fe929c8
JieZhan <178889339@qq.com>
2020-07-24 12:13:32 +0800

    row 和 col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="[`col-${span}`]"><slot></slot></div>
+</template>
+<script>
+    export default {
+        name:'GCol',
+        props:{
+            span:{
+                type:[Number,String]
+            }
+        }
+    }
+</script>
+<style scoped  lang="less">
+    .col{
+        width: 50%;
+        border: 1px solid red;
+        background-color: #666666;
+        min-height: 100px;
+    }
+    .cont(@count) when (@count < 25){
+        .cont(@count+1);
+        .col-@{count}{
+            width: (@count/24)*100%;
+        }
+    }
+    .cont(1);
+</style>
---
+++ src/row.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="row"><slot></slot></div>
+</template>
+<style scoped lang="less">
+    .row{
+        display: flex;
+    }
+</style>
