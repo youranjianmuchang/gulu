a9ab0939fafd10094f168650d7fabf20a8fb0e4c
JieZhan <178889339@qq.com>
2020-08-04 23:47:11 +0800

    collapse 基本样式和基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="collapseItem">
+        <div class="title" @click="open = !open">{{title}}</div>
+        <div class="content" v-if="open">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'GCollapseItem',
+        props:{
+            title:{
+                type:String,
+                required:true
+            }
+        },
+        data(){
+            return {
+                open:false
+            }
+        }
+    }
+</script>
+<style scoped lang="less">
+    @grey:#ddd;
+    @border-radius:4px;
+    .collapseItem{
+        > .title{
+            border:1px solid @grey;
+            margin: -1px -1px 0 -1px;
+            min-height: 32px;
+            display: flex;
+            align-items: center;
+            padding: 0 8px;
+        }
+        &:first-child{
+            > .title{
+                border-top-left-radius: @border-radius;
+                border-top-right-radius: @border-radius;
+            }
+        }
+        &:last-child{
+            > .title{
+                border-bottom-left-radius: @border-radius;
+                border-bottom-right-radius: @border-radius;
+            }
+        }
+        .content{
+            padding: 8px;
+        }
+    }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'GCollapse'
+    }
+</script>
+<style scoped lang="less">
+    @grey:#ddd;
+    @border-radius:4px;
+    .collapse{
+        border: 1px solid @grey;
+        border-radius: @border-radius;
+    }
+</style>
