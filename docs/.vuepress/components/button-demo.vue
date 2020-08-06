4795803acda7e1dfaa081fdd63e8fe2b7e9a5107
JieZhan <178889339@qq.com>
2020-08-06 14:06:31 +0800

    调整 button 组件说明

--- docs/.vuepress/components/button-demo.vue
+++ docs/.vuepress/components/button-demo.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
+        <h2>预览</h2>
         <g-button>默认样式</g-button>
         <g-button loading>加载中</g-button>
         <g-button icon="setting">带图标</g-button>
         <g-button icon="setting" icon-position="right">右边图标</g-button>
         <g-button disabled>禁用</g-button>
+        <h2>代码</h2>
         <pre><code>{{content}}</code></pre>
     </div>
 </template>
