1b2ad641f5bcd036aafd35e3a99b157292a63c62
JieZhan <178889339@qq.com>
2020-08-07 15:14:54 +0800

    完成 layout 组件说明

--- docs/.vuepress/components/layout-demo.vue
+++ docs/.vuepress/components/layout-demo.vue
@@ -1,31 +1,102 @@
 <template>
     <div>
-        <g-button>默认样式</g-button>
-        <g-button loading>加载中</g-button>
-        <g-button icon="setting">带图标</g-button>
-        <g-button icon="setting" icon-position="right">右边图标</g-button>
-        <g-button disabled>禁用</g-button>
-        <pre><code>{{content}}</code></pre>
+        <h2>预览</h2>
+        <h3>第一种</h3>
+        <g-layout>
+            <g-header class="header">header</g-header>
+            <g-content class="content">content</g-content>
+            <g-footer class="footer">footer</g-footer>
+        </g-layout>
+        <h3>代码</h3>
+        <pre><code>&nbsp;{{content1}}</code></pre>
+        <h3 style="margin-top: 100px">第二种</h3>
+        <g-layout>
+            <g-header class="header">header</g-header>
+            <g-layout>
+                <g-sider class="sider">sider</g-sider>
+                <g-content class="content">content</g-content>
+            </g-layout>
+            <g-footer class="footer">footer</g-footer>
+        </g-layout>
+        <h3>代码</h3>
+        <pre><code>&nbsp;{{content2}}</code></pre>
+        <h3 style="margin-top: 100px">第三种</h3>
+        <g-layout>
+            <g-sider class="sider">sider</g-sider>
+            <g-layout>
+                <g-header class="header">header</g-header>
+                <g-content class="content">content</g-content>
+                <g-footer class="footer">footer</g-footer>
+            </g-layout>
+        </g-layout>
+        <h3>代码</h3>
+        <pre><code>&nbsp;{{content3}}</code></pre>
     </div>
 </template>
 
 <script>
-    import Button from '../../../src/button'
+    import Sider from '../../../src/sider'
+    import Layout from '../../../src/layout'
+    import Content from "../../../src/content";
+    import Footer from "../../../src/footer";
+    import Header from "../../../src/header";
     export default {
         name: "button-demo.vue",
         components:{
-            'g-button':Button
+            'g-layout':Layout,
+            'g-footer': Footer,
+            'g-content': Content,
+            'g-sider': Sider,
+            'g-header': Header
         },
         data(){
             return {
-                content:`
-                    <g-button>默认样式</g-button>
-                    <g-button loading>加载中</g-button>
-                    <g-button icon="setting">带图标</g-button>
-                    <g-button icon="setting" icon-position="right">右边图标</g-button>
-                    <g-button disabled>禁用</g-button>
-                `.replace(/\t+| +/g,'').trim()
+                content1:`
+                    <g-layout>
+                        &nbsp;<g-header class="header">header</g-header>
+                        &nbsp;<g-content class="content">content</g-content>
+                        &nbsp;<g-footer class="footer">footer</g-footer>
+                    </g-layout>
+                `.replace(/\t+| +/g,' ').replace(/&nbsp;+/g,'    ').trim(),
+                content2:`
+                    <g-layout>
+                        &nbsp;<g-header class="header">header</g-header>
+                        &nbsp;<g-layout>
+                            &nbsp;&nbsp;<g-sider class="sider">sider</g-sider>
+                            &nbsp;&nbsp;<g-content class="content">content</g-content>
+                        &nbsp;</g-layout>
+                        &nbsp;<g-footer class="footer">footer</g-footer>
+                    </g-layout>
+                `.replace(/\t+| +/g,' ').replace(/&nbsp;+/g,'    ').trim(),
+                content3:`
+                    <g-layout>
+                        &nbsp;<g-sider class="sider">sider</g-sider>
+                        &nbsp;<g-layout>
+                            &nbsp;<g-header class="header">header</g-header>
+                            &nbsp;<g-content class="content">content</g-content>
+                            &nbsp;<g-footer class="footer">footer</g-footer>
+                        &nbsp;</g-layout>
+                    </g-layout>
+                `.replace(/\t+| +/g,' ').replace(/&nbsp;+/g,'    ').trim(),
             }
         }
     }
 </script>
+<style scoped>
+    .header{
+        background-color: red;
+        height: 50px;
+    }
+    .content{
+        background-color: yellow;
+        height: 50px;
+    }
+    .footer{
+        background-color: green;
+        height: 50px;
+    }
+    .sider{
+        background-color: grey;
+        width: 100px;
+    }
+</style>
